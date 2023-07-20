ac126a45d2cd4755d3f2e880242c2bb095082fa5
harsh7739 <hrbhumi123@gmail.com>
2023-07-20 23:52:57 +0530

    added

--- fashion-park/src/App.js
+++ fashion-park/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Routes/Navbar';
 import AllRoute from './Component/Routes/AllRoutes';
+import Home from './Component/Pages/Home';
 
 function App() {
   return (
     <div className="App">
    <h1>Welcome To The Fashion Park</h1>
-   <Navbar />
-  <AllRoute />
+   {/* <Navbar />
+  <AllRoute /> */}
+  <Home />
     </div>
   );
 }
